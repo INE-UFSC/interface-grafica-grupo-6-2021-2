519bd684b4d6983ea3123c1c80cf308ef6bbeea8
sammycosta <arminetah@hotmail.com>
2021-12-14 11:08:23 -0300

    Mudança de erro de digitaçao em CienteView

--- ClienteView.py
+++ ClienteView.py
@@ -1,26 +1,30 @@
-import PySimpleGUI as sg 
+import PySimpleGUI as sg
 
 # View do padrão MVC
+
+
 class ClienteView():
     def __init__(self, controlador):
         self.__controlador = controlador
         self.__container = []
-        self.__window = sg.Window("Consulta de clientes", self.__container ,font=("Helvetica", 14))
+        self.__window = sg.Window(
+            "Consulta de clientes", self.__container, font=("Helvetica", 14))
 
     def tela_consulta(self):
         sg.theme('Dark Brown 1')
 
         self.__container = [
-            [sg.Text('Digite o código ou o nome do cliente e clieque na ação esperada:')],
+            [sg.Text('Digite o código ou o nome do cliente e clique na ação esperada:')],
             [sg.Text('Nome:', size=(10, 1)), sg.InputText('', key='nome')],
             [sg.Text('Código:', size=(10, 1)), sg.InputText('', key='codigo')],
             [sg.Button('Cadastrar'), sg.Button('Consultar')],
             [sg.Text('', key='status', size=(50, 1))]
         ]
 
-        self.__window = sg.Window("Consulta de clientes", self.__container, font=("Helvetica", 14))
+        self.__window = sg.Window(
+            "Consulta de clientes", self.__container, font=("Helvetica", 14))
 
-    def mostra_resultado(self, resultado): 
+    def mostra_resultado(self, resultado):
         self.__window.Element('status').Update(resultado)
 
     def le_eventos(self):
